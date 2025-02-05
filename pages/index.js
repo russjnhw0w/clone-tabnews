ad9b47fdbac9b930b0c18e6a7d7cb33ee6c3846e
Adilson Nascimento <151871520+russjnhw0w@users.noreply.github.com>
2025-02-05 18:16:13 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mariana eu amo voce, se voce me ama da uma risadinha !!!!</h1>
+    return <h1>Mariana eu amo voce, se voce me ama da uma risadinha !!!! ;) </h1>
 }
 
 export default Home;
